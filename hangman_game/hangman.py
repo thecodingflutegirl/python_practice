4efac82ffbfe88f45d97e00807f84751931333fb
Julia Evers <thatflutegirl12@gmail.com>
2021-11-23 14:32:13 -0500

    removed todo lists- final code

--- hangman_game/hangman.py
+++ hangman_game/hangman.py
@@ -1,17 +1,13 @@
-# step 5
+
 from hangman_art import logo, stages
 import random
-
-
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-# Delete this line: word_list = ["ardvark", "baboon", "camel"]
 from hangman_words import word_list
-chosen_word = random.choice(word_list)
-print(f'this is the word: {chosen_word}')
-lives = 6
 
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 print(logo)
+
+chosen_word = random.choice(word_list)
+#print(f'HINT: this is the word: {chosen_word}')
+lives = 6
 
 display = []
 for char in chosen_word:
@@ -21,7 +17,7 @@ for char in chosen_word:
 game_over = False
 while not game_over:
     guess = input('Guess a letter: \n').lower()
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
+
     if guess in display:
         print(f"You've already guessed that letter: {guess}")
     # check guessed letter
@@ -31,7 +27,7 @@ while not game_over:
         if char == guess:
             display[i] = char
     if guess not in chosen_word:  # must be this way and can't be if guess != char because != is used for ints, not strings
-        # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+
         print(f'The letter {guess} is not in the word. You lose one life.')
         lives -= 1
 
@@ -45,7 +41,5 @@ while not game_over:
     if '_' not in display:
         game_over = True
         print('You win!')
-
-# TODO-2: - Import the stages from hangman_art.py and make this error go away.
 
     print(stages[lives])
