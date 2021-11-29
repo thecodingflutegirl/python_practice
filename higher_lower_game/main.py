498e842882eaa22450da88dda2742d061fc03ef1
Julia Evers <thatflutegirl12@gmail.com>
2021-11-28 20:49:06 -0500

    todo list of logic - how game works

---
+++ higher_lower_game/main.py
@@ -0,0 +1,12 @@
+from art import logo, vs
+from game_data import data
+
+# need to show logo
+# compare A - who is A? Along with their stats and where they're from
+# show the VS
+# "Against B:" Whoever B is with their stats and stuff
+# Ask user who has more followers? A or B?
+
+# if user is right their score goes up by 1 "You're right! Current score: {their score}"
+# and then you do another round of A vs B, re show logo and vs
+# if user is wrong the game ends and shows score "Sorry, that's wrong. Final score {the final or current? score}"
