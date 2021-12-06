963f2ba96a7df70754d8bc46447b00b1d3a51e2c
Julia Evers <thatflutegirl12@gmail.com>
2021-12-05 22:38:48 -0500

    etch a sketch turtle

---
+++ turtle_drawing_project/etch_sketch.py
@@ -0,0 +1,37 @@
+import turtle
+
+tim = turtle.Turtle()
+screen = turtle.Screen()
+tim.shape('classic')
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def counter_clockwise():
+    tim.right(10)
+
+
+def clockwise():
+    tim.left(10)
+
+
+def clear():
+    tim.penup()
+    tim.clear()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key='w', fun=move_forwards)
+screen.onkey(key='s', fun=move_backwards)
+screen.onkey(key='a', fun=counter_clockwise)
+screen.onkey(key='d', fun=clockwise)
+screen.onkey(key='c', fun=clear)
+screen.exitonclick()
