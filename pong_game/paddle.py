a9a4220256b038b4df328645fdff83daff11ed9b
Julia Evers <thatflutegirl12@gmail.com>
2021-12-08 17:08:11 -0500

    full pong game

---
+++ pong_game/paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle 
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape('square')
+        self.shapesize(5, 1)
+        self.color('white')
+        self.penup()
+        self.goto(position)
+        #self.setheading(90)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
