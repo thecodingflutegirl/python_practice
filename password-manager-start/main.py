2cefc47748bc2cf707d22b5d74d2ad3828007759
Julia Evers <thatflutegirl12@gmail.com>
2021-12-18 17:57:05 -0500

    updated json use

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -32,20 +33,56 @@ def save_data():
     website = website_input.get()
     email = email_input.get()
     password = password_input.get()
+    new_data = {website: {
+        'email': email,
+        'password': password
+    }}
 
     if website and email and password:
-        is_ok = messagebox.askokcancel(
-            title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
+        # is_ok = messagebox.askokcancel(
+        #     title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
 
-        if is_ok:
-            with open('data.txt', 'a') as data:
-                data.write(f"{website} | {email} | {password}\n")
-                website_input.delete(0, END)
-                email_input.delete(0, END)
-                password_input.delete(0, END)
+        # if is_ok:
+        try:
+            with open('data.json', 'r') as data_file:
+                # REading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+            with open('data.json', 'w') as data_file:
+                # SAving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_input.delete(0, END)
+            email_input.delete(0, END)
+            password_input.delete(0, END)
     else:
         messagebox.showinfo(
             title='WARNING', message="Don't leave any fields empty!")
+
+# -------------------------- Find Password ---------------------------- #
+
+
+def find_password():
+    website = website_input.get()
+    try:
+        with open('data.json', 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='No Data File Found')
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(
+                title=website, message=f'Email/Username: {email} \nPassword: {password}')
+        else:
+            messagebox.showinfo(title='Cannot find Information',
+                                message=f'No details for {website} exists')
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -61,8 +98,8 @@ canvas.grid(column=1, row=0)
 website_label = Label(text='Website:')
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = Entry(width=20)
+website_input.grid(column=1, row=1)
 website_input.focus()
 
 email_label = Label(text='Email/Username:')
@@ -83,6 +120,8 @@ make_password.grid(column=2, row=3)
 add_btn = Button(text="Add", width=35, command=save_data)
 add_btn.grid(column=1, row=4, columnspan=2)
 
+search_btn = Button(text="Search", width=12, command=find_password)
+search_btn.grid(column=2, row=1)
 
 window.mainloop()
 # I would like to edit this for some of the colors but its not a high priority
